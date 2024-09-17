2ac7e39aaaf97a7ff84e4890b1a1ab9a47b943b2
Cheshire Cat <cheshire@mbp-de-cheshire.home>
2024-09-17 10:29:51 +0200

    Fondement Prog Exo

---
+++ Fondement Programmation/exercices.py
@@ -0,0 +1,20 @@
+# 17.09.24
+
+nb_heure_aout = 31 * 24
+print(nb_heure_aout)
+
+print(5**4)
+
+nb_bus = 333 % 20
+print(333//20+1)
+
+print(nb_bus)
+
+MAX_POIDS = 5000
+charge_initiale = 35 * 54.5 + 40 * 35
+print(MAX_POIDS - charge_initiale)
+charge_laus = charge_initiale + 150 * 5
+print(MAX_POIDS - charge_laus)
+charge_mont = charge_laus + 29 * 12.5
+print(MAX_POIDS - charge_mont)
+
