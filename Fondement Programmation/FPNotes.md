1540204e3397aaa6963fa2b0e1ba3e2c0af579e7
Cheshire Cat <cheshire@mbp-de-cheshire.home>
2024-09-17 18:03:30 +0200

    Fondament Progr exo + theorie 17.09.24

---
+++ Fondement Programmation/FPNotes.md
@@ -0,0 +1,22 @@
+# 17.09.24
+
+**Exercice données :**
+    Température             Nombre (float)
+    Nom                     Chaine de char
+    Année de naissance     \Nombre (int)
+    Statut d’inscription \\\Bool
+    Date bachelor         \\Date
+    Numero postal         \\Nombre (int)
+    Adresse                 Objet
+    Client                 \Objet
+    Mois                 \\\Nombre (int)
+    Liste des modules     \\Liste objet
+
+**Constante :** valeur qui ne change pas lors de l’exécution de l’algorithme
+**Variable :** valeur qui va varier pendant l’exécution de l’algorithme
+
+**Convention :**
+- Espace autour des opérations
+- Espace après les deux points du type
+- Mettre le type au variable
+- Variable minuscule avec _ entre les mots
--- Fondement Programmation/exercices.py
+++ Fondement Programmation/exercices.py
@@ -21,5 +21,31 @@ print(MAX_POIDS - charge_initiale)
 charge_laus: float = charge_initiale + 150 * 5
 print(MAX_POIDS - charge_laus)
 charge_mont: float = charge_laus + 29 * 12.5
-print(MAX_POIDS - charge_mont)
+print(MAX_POIDS - charge_mont, "\n")
 
+# Serie I
+print("Somme")
+x: int = int(input("X : "))
+y: int = int(input("Y : "))
+print(x + y, "\n")
+
+print("Multiplication")
+v: int = int(input("v : "))
+w: int = int(input("w : "))
+print(v * w, "\n")
+
+print("Moyenne")
+a: int = int(input("a : "))
+b: int = int(input("b : "))
+c: int = int(input("c : "))
+print(int((a + b + c) / 3), "\n")
+
+print("Surface")
+l: int = int(input("l : "))
+w: int = int(input("w : "))
+print(l * w, "\n")
+
+print("Division et reste")
+x: int = int(input("X : "))
+y: int = int(input("Y : "))
+print(int(x / y), int(x % y))
