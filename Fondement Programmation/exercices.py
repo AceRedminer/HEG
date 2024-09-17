f4a4dbb53522abd8dcf52a495d55708218b372d8
Cheshire Cat <cheshire@mbp-de-cheshire.home>
2024-09-17 10:52:37 +0200

    ...

--- Fondement Programmation/exercices.py
+++ Fondement Programmation/exercices.py
@@ -1,20 +1,25 @@
 # 17.09.24
 
+# Heure en aout
 nb_heure_aout: int = 31 * 24
 print(nb_heure_aout)
 
+# 5 puissance 4
 print(5**4)
 
+# Combien de bus pour distribuer 333 personnes avec capacite max de 20
 nb_bus: int = 333 % 20
 print(333//20+1)
 
+# Nombre de personne dans le dernier bus
 print(nb_bus)
 
+# Exercices Perrotte
 MAX_POIDS: int = 5000
-charge_initiale: int = 35 * 54.5 + 40 * 35
+charge_initiale: float = 35 * 54.5 + 40 * 35
 print(MAX_POIDS - charge_initiale)
-charge_laus: int = charge_initiale + 150 * 5
+charge_laus: float = charge_initiale + 150 * 5
 print(MAX_POIDS - charge_laus)
-charge_mont: int = charge_laus + 29 * 12.5
+charge_mont: float = charge_laus + 29 * 12.5
 print(MAX_POIDS - charge_mont)
 
