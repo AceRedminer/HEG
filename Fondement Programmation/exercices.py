496289b48ddcf970d6de6b6677a800a02bd4a1c2
Cheshire Cat <cheshire@mbp-de-cheshire.home>
2024-09-17 10:49:47 +0200

    stupidly putting types on variables in python

--- Fondement Programmation/exercices.py
+++ Fondement Programmation/exercices.py
@@ -1,20 +1,20 @@
 # 17.09.24
 
-nb_heure_aout = 31 * 24
+nb_heure_aout: int = 31 * 24
 print(nb_heure_aout)
 
 print(5**4)
 
-nb_bus = 333 % 20
+nb_bus: int = 333 % 20
 print(333//20+1)
 
 print(nb_bus)
 
-MAX_POIDS = 5000
-charge_initiale = 35 * 54.5 + 40 * 35
+MAX_POIDS: int = 5000
+charge_initiale: int = 35 * 54.5 + 40 * 35
 print(MAX_POIDS - charge_initiale)
-charge_laus = charge_initiale + 150 * 5
+charge_laus: int = charge_initiale + 150 * 5
 print(MAX_POIDS - charge_laus)
-charge_mont = charge_laus + 29 * 12.5
+charge_mont: int = charge_laus + 29 * 12.5
 print(MAX_POIDS - charge_mont)
 
