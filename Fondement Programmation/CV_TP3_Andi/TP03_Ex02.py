fb3012997ac0591208f28cf359b33b0753346602
Cheshire <costanti.volta@hes-so.ch>
2024-11-19 11:36:40 +0100

    TP03 Ex02 done

--- Fondement Programmation/CV_TP3_Andi/TP03_Ex02.py
+++ Fondement Programmation/CV_TP3_Andi/TP03_Ex02.py
@@ -5,10 +5,10 @@ import TP03_modules
 ### Déclaration et Initialisation des variables
 
 MOT: str = tirer_mot()
-
+lettre_tirées: str = ""
+nb_erreur_restantes: int = 10
 
 #Programme Principal
 if __name__ == "__main__":
     print(MOT)
-    lettres_entree: str = input("Veuillez rentrer une lettre a essayer : ")
-    TP03_modules.affichage_mot_cache(MOT, lettres_entree, 10)
+    TP03_modules.affichage_mot_cache(MOT, lettre_tirées, nb_erreur_restantes)
--- Fondement Programmation/CV_TP3_Andi/TP03_modules.py
+++ Fondement Programmation/CV_TP3_Andi/TP03_modules.py
@@ -11,23 +11,47 @@ def test_porte_bonheur(_nombre_choisi: int) -> bool:
         nb_str = str(nb_tmp)
     return nb_tmp < 10
 
-def affichage_mot_cache(_mot_cache: str, _lettres_tires: str, _nb_erreurs_restantes: int):
-    mot_lettre_tires: str = ""
-    for i in range(len(_mot_cache)):
-        check_lettre: tuple = trouver_lettre(_mot_cache[i], _lettres_tires)
-        if(check_lettre[0]):
-            mot_lettre_tires += check_lettre[1]
-        else:
-            mot_lettre_tires += '_'
-    print(mot_lettre_tires)
+def affichage_mot_cache(_mot_cache: str, _lettres_tirees: str, _nb_erreurs_restantes: int):
+    # On cree la variable qui va stocker le mot avec les lettres decouverte
+    mot_lettre_tirees: str = ""
+    # On parcours le mot caché et on vérifie lettre par lettre qu'elle appartient dans 
+    # la liste des lettre tirés sinon on mets un tiret du bas
+    a_trouver_une_lettre = False
+    while _nb_erreurs_restantes != 0:
+        _lettres_tirees = tirage_lettre(_lettres_tirees)
 
-def tirage_lettre(_lettre_tires: str):
-    lettre: str = input("Veuillez saisir une lettre : ")
-    while(trouver_lettre(lettre, _lettre_tires)[0] == True):
-        lettre: str = input("Veuillez saisir une lettre : ")
+        for i in range(len(_mot_cache)):
+            if _mot_cache[i] in _lettres_tirees:
+                mot_lettre_tirees += _mot_cache[i]
+                a_trouver_une_lettre = True
+            else:
+                mot_lettre_tirees += '_'
         
-def trouver_lettre(lettre, liste):
-    for mot in liste:
-        if lettre in mot:
-            return (True, lettre)
-    return (False, None)
+        if a_trouver_une_lettre == False:
+            _nb_erreurs_restantes = diminuer_erreurs(_nb_erreurs_restantes)
+        print(f"{_nb_erreurs_restantes} erreurs restantes")
+        if _mot_cache == mot_lettre_tirees:
+            print(f"Vous avez gagné en {10 - _nb_erreurs_restantes} tentatives ! Le mot était {_mot_cache}")
+            break
+        print(mot_lettre_tirees)
+        mot_lettre_tirees = ""
+
+def tirage_lettre(_lettres_tirees: str) -> str:
+    # On récupere la lettre rentrée par l'utilisateur et on l'ajoute a la liste des lettre tirées
+    lettre: str = input("Veuillez saisir une lettre : ")
+    # On boucle tant que lettre apparait dans _lettres_tirees
+    while lettre in _lettres_tirees:
+        lettre: str = input("Veuillez saisir une lettre : ")
+
+    _lettres_tirees += lettre
+    return _lettres_tirees
+
+def diminuer_erreurs(_nb_erreurs_restantes: int) -> int: 
+    _nb_erreurs_restantes -= 1
+    return _nb_erreurs_restantes
+
+def verifier_mot(_mot_cache: str, _lettres_tirees: str) -> bool:
+    for lettre in _mot_cache:
+        if lettre not in _lettres_tirees:
+            return False  # Si une lettre n'a pas été trouvée, pas encore gagné
+    return True  # Toutes les lettres sont trouvées
