bc4f74492ccfe380fd5b50020c394fd32107e79c
Cheshire <costanti.volta@hes-so.ch>
2024-11-27 13:08:32 +0100

    TP4 5 fonctions done

--- Fondement Programmation/CV_TP4_Andi/TP4_ex1.py
+++ Fondement Programmation/CV_TP4_Andi/TP4_ex1.py
@@ -5,3 +5,16 @@ liste_nb : list = [3, 12, 30, 8, 4, 9, 7, 15, 10, 1, 21, 13, 12, 27, 3, 14]
 if __name__ == "__main__":
     #Do something
     print(TP4_modules.affichage_liste_str(liste_nb))
+    print("------------")
+
+    TP4_modules.affichage_liste_colonne(liste_nb)
+    print("------------")
+
+    TP4_modules.multiple_liste(liste_nb, 3)
+    print("------------")
+
+    TP4_modules.plus_petit_chiffre(liste_nb)
+    print("------------")
+
+    TP4_modules.somme_impair(liste_nb)
+    print("------------")
--- Fondement Programmation/CV_TP4_Andi/TP4_modules.py
+++ Fondement Programmation/CV_TP4_Andi/TP4_modules.py
@@ -8,3 +8,34 @@ def affichage_liste_str(_liste_nb: list) -> str:
     for nb in _liste_nb:
         return_str += str(nb) + " "
     return return_str
+
+def affichage_liste_colonne(_liste_nb: list) -> None:
+    j: int = 0
+    indexs: str = ""
+    nb_str: str = ""
+    for i in _liste_nb:
+        indexs += str(j) + " | "
+        nb_str += str(i) + " | " 
+        j += 1
+    
+    print("Indice : " + indexs.rstrip(" | "))
+    print("Valeur : " + nb_str.rstrip(" | "))
+
+def multiple_liste(_liste_nb: int, _chiffre: int) -> None:
+        idx: int = 0
+        for i in _liste_nb:
+             _liste_nb[idx] = i * 3
+             idx += 1
+        affichage_liste_none(_liste_nb)
+
+def plus_petit_chiffre(_liste_nb: list):
+    _liste_nb.sort()
+    affichage_liste_none(_liste_nb)
+
+def somme_impair(_liste_nb: list) -> None:
+    somme_nb: int = 0
+    for i in _liste_nb:
+        if i % 2 != 0:
+            somme_nb += i
+    print(str(somme_nb))
+
